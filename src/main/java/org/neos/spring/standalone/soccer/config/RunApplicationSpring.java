87f613d8c21cdbe6f65ceedd15ebc6e53beebd9f
hhugohm <hhugohm@gmail.com>
2018-11-12 09:55:13 -0600

    clean up dead code

--- src/main/java/org/neos/spring/standalone/soccer/config/RunApplicationSpring.java
+++ src/main/java/org/neos/spring/standalone/soccer/config/RunApplicationSpring.java
@@ -2,7 +2,6 @@ package org.neos.spring.standalone.soccer.config;
 
 import org.neos.spring.standalone.soccer.business.Game;
 import org.neos.spring.standalone.soccer.business.Team;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class RunApplicationSpring {
