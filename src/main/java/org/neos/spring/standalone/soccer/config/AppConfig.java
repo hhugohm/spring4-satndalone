980c2be9bb819d374813406fe1a8625e7bcc2b55
hhugohm <hhugohm@gmail.com>
2018-11-11 20:02:00 -0600

    update Scope annotation

--- src/main/java/org/neos/spring/standalone/soccer/config/AppConfig.java
+++ src/main/java/org/neos/spring/standalone/soccer/config/AppConfig.java
@@ -2,7 +2,6 @@ package org.neos.spring.standalone.soccer.config;
 
 import org.neos.spring.standalone.soccer.business.*;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
